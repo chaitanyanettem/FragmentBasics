40b4a285dbfd148565400026453ee9231cf64f64
Chaitanya Nettem <chaitanyanettem@gmail.com>
2016-07-15 23:21:31 +0530

    removed main_activity layout. modified onAttach to remove deprecated method.

--- app/src/main/java/com/example/arrayjumper/FragmentBasics/HeadlinesFragment.java
+++ app/src/main/java/com/example/arrayjumper/FragmentBasics/HeadlinesFragment.java
@@ -17,6 +17,7 @@
 package com.example.arrayjumper.FragmentBasics;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
@@ -57,15 +58,15 @@ public class HeadlinesFragment extends ListFragment {
     }
 
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
+    public void onAttach(Context context) {
+        super.onAttach(context);
 
         // This makes sure that the container activity has implemented
         // the callback interface. If not, it throws an exception.
         try {
-            mCallback = (OnHeadlineSelectedListener) activity;
+            mCallback = (OnHeadlineSelectedListener) context;
         } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
+            throw new ClassCastException(context.toString()
                     + " must implement OnHeadlineSelectedListener");
         }
     }
